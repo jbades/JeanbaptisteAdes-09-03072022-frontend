a55ebae70b45ac86c4d87e5bd2b9cd29ba9492a1
jbades <jbades@majba.com>
2023-01-20 18:03:21 +0100

    cleaning code

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -87,22 +87,18 @@ export default class {
   }
 
   handleEditTicket(e, bill, bills) {
-    // console.log("compteur début= ", this.counter, " - this.id=", this.id, " - bill.id=", bill.id);
     if (this.counter === undefined || this.id !== bill.id) this.counter = 0
     if (this.id === undefined || this.id !== bill.id) this.id = bill.id
-    // console.log("compteur début= ", this.counter, " - this.id=", this.id, " - bill.id=", bill.id);
     if (this.counter % 2 === 0) {
-      // console.log("compteur après ràz=", this.counter, " ; modulo=", this.counter % 2);
       bills.forEach(b => {
         $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
       })
       $(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
       $('.dashboard-right-container div').html(DashboardFormUI(bill))
       $('.vertical-navbar').css({ height: '150vh' })
-      // this.counter ++
-      // console.log("compteur après incrément=", this.counter);
+      // setting up counting on bills display issue
+      // // this.counter ++
     } else {
-      // console.log("compteur après ràz=", this.counter, " ; modulo=", this.counter % 2);
       $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
 
       $('.dashboard-right-container div').html(`
@@ -110,7 +106,6 @@ export default class {
       `)
       $('.vertical-navbar').css({ height: '120vh' })
       // this.counter ++
-      // console.log("compteur après incrément=", this.counter);
     }
     $('#icon-eye-d').click(this.handleClickIconEye)
     $('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill))
@@ -138,24 +133,18 @@ export default class {
   }
 
   handleShowTickets(e, bills, index) {
-    // setting up counting on bills display issue
-    // console.log("compteur début= ", this.counter);
     if (this.counter === undefined || this.index !== index) this.counter = 0
     if (this.index === undefined || this.index !== index) this.index = index
     if (this.counter % 2 === 0) {
-      // console.log("compteur après ràz=", this.counter, " ; modulo=", this.counter % 2);
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
       $(`#status-bills-container${this.index}`)
         .html(cards(filteredBills(bills, getStatus(this.index))))
       this.counter ++
-      // console.log("compteur après incrément=", this.counter);
     } else {
-      // console.log("compteur apèrs ràz=", this.counter, " ; modulo=", this.counter % 2);
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
       $(`#status-bills-container${this.index}`)
         .html("")
       this.counter ++
-      // console.log("compteur après incrément=", this.counter);
     }
 
     bills.forEach(bill => {
